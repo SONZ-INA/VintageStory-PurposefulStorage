2f54eccaac0a103eed3a9befdafe448a80da31ed
SONZ-INA <aleksakostic034@gmail.com>
2025-06-29 13:57:31 +0200

    1.3.0 - Minor code upgrades
    - Pants and Shoulderware whitelisted for the Wardrobe.
    - Automated discovery of restriction.json files.
    - Added BlacklistedCodes to the RestrictionData.
    - Added Ukranian translation.
    - Edited Wardrobe shape to have shorter "sticks".

--- code/Data/RestrictionData.cs
+++ code/Data/RestrictionData.cs
@@ -3,5 +3,6 @@
 public class RestrictionData {
     public string[] CollectibleTypes { get; set; }
     public string[] CollectibleCodes { get; set; }
+    public string[] BlacklistedCodes { get; set; }
     public Dictionary<string, string[]> GroupingCodes { get; set; }
 }
